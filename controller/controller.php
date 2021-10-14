c1270678c0cf5ebc072d66997adf375d265dbd1b
remib74 <33649152+remib74@users.noreply.github.com>
2021-10-14 02:47:21 +0200

    all the views are set to uset frontEnd.php as view

--- controller/controller.php
+++ controller/controller.php
@@ -12,33 +12,95 @@ function contactImages()
 function dessins()
 {
 
-    require('view/dessin.php');
+    require('model/siteConfig.php');         
+    //echo ($ImagesAll);
+    $sqlDessins = "SELECT * FROM m3_prod WHERE rubrique='dessin' ORDER BY id";
+    foreach ($conn->query($sqlDessins) as $row) {
+        $rowid = $row['id'];
+        $rowRub = $row['rubrique'];
+        $rowTit = $row['title'];
+        $rowFor = $row['format'];
+        $rowPri = $row['prix'];
+        
+
+require('view/frontEnd.php');
+
+}
     
 }
 function infographies()
 {
 
-    require('view/infographie.php');
+    require('model/siteConfig.php');         
     
+    $sqlInfographies = "SELECT * FROM m3_prod WHERE rubrique='infographie' ORDER BY id";
+    foreach ($conn->query($sqlInfographies) as $row) {
+        $rowid = $row['id'];
+        $rowRub = $row['rubrique'];
+        $rowTit = $row['title'];
+        $rowFor = $row['format'];
+        $rowPri = $row['prix'];
+        
+
+require('view/frontEnd.php');
+    
+}
 }
 
 function peintures()
 {
 
-    require('view/peinture.php');
+    require('model/siteConfig.php');         
     
+    $sqlPeintures = "SELECT * FROM m3_prod WHERE rubrique='peinture' ORDER BY id";
+    foreach ($conn->query($sqlPeintures) as $row) {
+        $rowid = $row['id'];
+        $rowRub = $row['rubrique'];
+        $rowTit = $row['title'];
+        $rowFor = $row['format'];
+        $rowPri = $row['prix'];
+        
+
+require('view/frontEnd.php');
+    
+}
 }
 function pastels()
 {
 
-    require('view/pastel.php');
+    require('model/siteConfig.php');         
     
+    $sqlPastels = "SELECT * FROM m3_prod WHERE rubrique='pastel' ORDER BY id";
+    foreach ($conn->query($sqlPastels) as $row) {
+        $rowid = $row['id'];
+        $rowRub = $row['rubrique'];
+        $rowTit = $row['title'];
+        $rowFor = $row['format'];
+        $rowPri = $row['prix'];
+        
+
+require('view/frontEnd.php');
+    
+}
 }
 function sculptures()
 {
 
-    require('view/sculpture.php');
+
+    require('model/siteConfig.php');         
     
+    $sqlSculptures = "SELECT * FROM m3_prod WHERE rubrique='sculpture' ORDER BY id";
+    foreach ($conn->query($sqlSculptures) as $row) {
+        $rowid = $row['id'];
+        $rowRub = $row['rubrique'];
+        $rowTit = $row['title'];
+        $rowFor = $row['format'];
+        $rowPri = $row['prix'];
+        
+
+require('view/frontEnd.php');
+    
+}
 }
 
 function listImages()
